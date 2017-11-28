74484e9349eef4d1f531deaf6d649d68b09e6b6f
Stanley Cheung <hi@stanley.bz>
2017-11-28 19:45:31 +0800

    Update Handler.php

--- app/app/Exceptions/Handler.php
+++ app/app/Exceptions/Handler.php
@@ -47,10 +47,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        // if($e instanceof NotFoundHttpException || $e instanceof MethodNotAllowedHttpException)
-        // {
-        //     return response('', 400);
-        // }
+        if($e instanceof NotFoundHttpException || $e instanceof MethodNotAllowedHttpException)
+        {
+            return response('', 400);
+        }
         return parent::render($request, $e);
     }
 }
